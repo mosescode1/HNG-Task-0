b273e677f0e34f07554b2daaf0a31f33a26f3755
mosescode1 <emosescode@gmail.com>
2025-01-31 22:52:40 +0100

    Add main.go to implement a simple Fiber web server

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+
+	app := fiber.New()
+	now := time.Now()
+
+	isoformat := now.Format(time.RFC3339)
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{
+			"email":            "iammoses19@gmail.com",
+			"current_datetime": isoformat,
+			"github_url":       "https://github.com/mosescode1/HNG-Task-0.git",
+		})
+	})
+
+	log.Fatal(app.Listen(":3000"))
+}
